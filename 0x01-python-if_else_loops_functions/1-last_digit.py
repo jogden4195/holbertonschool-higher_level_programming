2701b051f346d0a1beebffe19f9a70f3cb01f049
Jenn <jennogden95@gmail.com>
2018-09-11 19:43:45 +0000

    Fixed: Whenever it's a negative number it will now have a negative last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,14 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = int(str(number)[-1])
+if number < 0:
+	last_digit = last_digit * -1
 
-# str_to_print = "Last digit of {:d} is {:d} ".format(number, last_digit)
+str_to_print = "Last digit of {:d} is {:d} ".format(number, last_digit)
 
 if last_digit > 5:
-    end = "is greater than 5"
+    str_to_print += "and is greater than 5"
 elif last_digit == 0:
-    end = "is 0"
+    str_to_print += "and is 0"
 else:
-    end = "is less than 6 and not 0"
+    str_to_print += "and is less than 6 and not 0"
 
-print("Last digit of {:d} is {:d} and {}".format(number, last_digit, end))
+print(str_to_print)
