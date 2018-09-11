1cca53e080f233af34587c6abc49bdfc7d1ad3b3
Jenn <jennogden95@gmail.com>
2018-09-11 19:13:54 +0000

    Fixed: Changed print statement.

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-i = '0'
-
-while (ord(i) - 48) <= 99:
-    print(str(ord(i) - 48) + ' = ' + str(hex(ord(i) - 48)))
-    i = chr(ord(i) + 1)
+for i in range(99):
+    print("{:d} = {}".format(i, hex(i)))
