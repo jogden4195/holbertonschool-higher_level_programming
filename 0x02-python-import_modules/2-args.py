0eb99528cfdf0f17793effa16cb012fa4eadc670
Jenn <jennogden95@gmail.com>
2018-09-13 18:33:55 +0000

    Added name == name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-print("{:d} arguments".format(len(sys.argv[1:])), end="")
-if (len(sys.argv[1:])) == 0:
-    print(".")
-else:
-    print(":")
-    for i in sys.argv[1:]:
-        print("{:d}: {}".format(sys.argv.index(i), i))
+if __name__ == "__main__":
+    print("{:d} arguments".format(len(sys.argv[1:])), end="")
+    if (len(sys.argv[1:])) == 0:
+        print(".")
+    else:
+        print(":")
+        for i in sys.argv[1:]:
+            print("{:d}: {}".format(sys.argv.index(i), i))
