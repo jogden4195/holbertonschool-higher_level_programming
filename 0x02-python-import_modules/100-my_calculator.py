f9aabbc621248e1d88db57495cc2f38e627e9141
Jenn <jennogden95@gmail.com>
2018-09-13 18:39:52 +0000

    Fixed pep8 errors.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     op = sys.argv[2]
 
     if op == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif op == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a,b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif op == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif op == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a,b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
