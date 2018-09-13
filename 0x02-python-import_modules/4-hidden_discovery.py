c3850b839579cb2453b1f4f99c2f2d94ff7745e2
Jenn <jennogden95@gmail.com>
2018-09-13 19:25:42 +0000

    Fixed: print statement so we're just printing str(i) and also increased index from [:1] to [:2]

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ import hidden_4
 
 if __name__ == "__main__":
     for i in dir(hidden_4):
-        if i[:1] != "__":
-            print("{}", i)
+        if i[:2] != "__":
+            print(str(i))
