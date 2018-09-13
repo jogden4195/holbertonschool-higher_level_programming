6a0552f3f539bf46988234fd2177a5d2ab566041
Jenn <jennogden95@gmail.com>
2018-09-13 20:29:30 +0000

    Checks for valid op at beginning of program.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,7 @@ import sys
 from calculator_1 import add, sub, div, mul
 
 if __name__ == "__main__":
+
     if len(sys.argv[1:]) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -10,8 +11,12 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     op = sys.argv[2]
+    valid = ["+", "-", "*", "/"]
 
-    if op == "+":
+    if op not in valid:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    elif op == "+":
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif op == "-":
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
@@ -19,6 +24,3 @@ if __name__ == "__main__":
         print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif op == "/":
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
