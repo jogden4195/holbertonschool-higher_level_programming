81b37bba47265d9d8830a9406081957f5408a780
Jenn <jennogden95@gmail.com>
2018-09-20 19:19:55 +0000

    Fixed: Changed code back to original submission. There was nothing wrong with it other than missing the shebang.

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for k in sorted(a_dictionary.keys()):
-        print(k + ": " + str(a_dictionary[k]))
+    for k, v in sorted(a_dictionary.items()):
+        print("{:s}: {}".format(k, v))
