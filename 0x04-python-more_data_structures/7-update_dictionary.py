b382be0b227ce9b6afe132819737d12388589914
Jenn <jennogden95@gmail.com>
2018-09-20 19:47:46 +0000

    Fixed: Removed None check and added a return statement

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is None:
-        a_dictionary = dict(key=value)
-    else:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
+    return a_dictionary
