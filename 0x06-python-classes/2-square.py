ea3542ab91de123186dde1ac97aa759ade671453
Jenn <jennogden95@gmail.com>
2018-10-03 02:51:30 +0000

    Adding task 2.

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """Square - defines a square"""
+    def __init__(self, size=0):
+        """ __init__ - initializes the object
+
+        Attributes:
+            size (int): size of the square
+
+        """
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
