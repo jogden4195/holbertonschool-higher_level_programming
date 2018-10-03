5036c57c2c203b7093cff0cdde2e9f3528c1302e
Jenn <jennogden95@gmail.com>
2018-10-03 01:44:54 +0000

    Adding task 1.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Square - defines a square"""
+    def __init__(self, size):
+        """ __init__ - initializes the object
+
+        Attributes:
+            size: size of the square
+
+        """
+        self.__size = size
