376a9a3553e91798fbad4cb7d097810ca2a3b829
Jenn <jennogden95@gmail.com>
2018-10-04 00:04:29 +0000

    Adding task 0.

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """ add_integer - adds 2 integers
+
+    Args:
+        a (int): The first integer we are adding
+        b (int): The second integer we are adding
+
+    Returns:
+        the sum of a and b
+
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
