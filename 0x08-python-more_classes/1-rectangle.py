869e63ff8df4ca4a7285d59c9733a1e3b269c4e0
Jenn <jennogden95@gmail.com>
2018-10-08 23:42:56 +0000

    Adding task 1, creating private instance attributes width and height.

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+class Rectangle:
+    """ Rectangle - Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """ __init - initializes the rectangle
+
+        Attributes:
+            width: how wide the rectangle will be
+            height: how tall the rectangle will be
+
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        """ width - gets the width attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height - gets the height attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
