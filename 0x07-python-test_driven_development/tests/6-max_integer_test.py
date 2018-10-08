e0bfedf0dde082dd61234181ad69a812de89b29b
Jenn <jennogden95@gmail.com>
2018-10-08 00:12:00 +0000

    Fixed test for negative max

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,8 +17,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 3, 2]), 3)
         self.assertEqual(max_integer([1, 5, 3, 2, 6, 3, 9]), 9)
         self.assertEqual(max_integer([9, 3, 6, 2, 3, 5, 1]), 9)
-        self.assertEqual(max_integer([-4, -9, 5, -2, 3, 1]), 5)
-        self.assertEqual(max_integer([-1, 3, 4, 5, 5]), 5)
+        self.assertEqual(max_integer([-4, -9, -5, -2, -3, -1]), -1)
         self.assertEqual(max_integer([1]), 1)
 
     def test_errors(self):
