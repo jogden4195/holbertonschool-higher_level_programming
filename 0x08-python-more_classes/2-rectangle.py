4776c1e4080401df7b4d118dee488272a88f6710
Jenn <jennogden95@gmail.com>
2018-10-09 07:24:57 +0000

    Fixed perimeter so when either width or height is 0, method returns 0.

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,15 +9,7 @@ class Rectangle:
             height: how tall the rectangle will be
 
         """
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         self.__width = width
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.__height = height
 
     @property
@@ -52,4 +44,6 @@ class Rectangle:
 
     def perimeter(self):
         """ perimeter - returns the current rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (2 * self.__width) + (2 * self.__height)
