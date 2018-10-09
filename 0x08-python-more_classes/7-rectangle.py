49f12c346646bf763754706fbd5504238dddbfa9
Jenn <jennogden95@gmail.com>
2018-10-09 07:06:07 +0000

    Fixed pep8 errors.

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -3,6 +3,7 @@ class Rectangle:
     """ Rectangle - Defines a rectangle"""
     number_of_instances = 0
     print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         """ __init__ - initializes the rectangle
 
