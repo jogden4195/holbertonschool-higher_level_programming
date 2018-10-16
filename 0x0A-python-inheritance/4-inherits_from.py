080524315dafe613fc3acc8c7c13acf7025d7b40
Jenn <jennogden95@gmail.com>
2018-10-16 09:46:27 +0000

    Fixed shebang

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 """ Module that checks if obj is subclass """
 
 
 def inherits_from(obj, a_class):
     """
-
+    inherits_from - Checks if an obj is a subclass
     """
     if issubclass(type(obj), a_class) and (type(obj) != a_class):
         return True
