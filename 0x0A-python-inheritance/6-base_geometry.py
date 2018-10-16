88ca0333214392cf11259db9705a50607d0e1335
Jenn <jennogden95@gmail.com>
2018-10-16 00:11:13 +0000

    Adding task 6.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """ BaseGeometry - Defines a Based Shape """
+
+    def area(self):
+        """ area - raises an exception """
+        raise Exception("area() is not implemented")
