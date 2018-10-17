6657aa274823adc39d5f48f445b37c29f49366af
Jenn <jennogden95@gmail.com>
2018-10-17 04:10:54 +0000

    Importing BaseGeometry class from 7.

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
 
-
-class BaseGeometry:
-    """ BaseGeometry - Defines a Based Shape """
-
-    def area(self):
-        """ area - raises an exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ integer_validator - validates a value
-
-        Arguments:
-            name (str): name
-            value (int): value
-
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
