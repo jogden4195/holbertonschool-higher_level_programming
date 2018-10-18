afdde862319b849f71d9963d0d5f78b10b9f37aa
Jenn <jennogden95@gmail.com>
2018-10-18 21:43:06 +0000

    Updated rectangle class by adding validation of all setter methods and instantiation (uid=1000(vagrant) gid=1000(vagrant) groups=1000(vagrant) excluded)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,6 +18,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, size):
         """ width - sets the width of the rectangle """
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
         self.__width = size
 
     @property
@@ -27,6 +31,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, size):
         """ height - sets the height of the rectangle """
+        if type(size) is not int:
+            raise TypeError("height must be an integer")
+        if size <= 0:
+            raise ValueError("height must be > 0")
         self.__height = size
 
     @property
@@ -36,6 +44,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, coord):
         """ x - sets the x coord. of the rectangle """
+        if type(coord) is not int:
+            raise TypeError("x must be an integer")
+        if coord < 0:
+            raise ValueError("x must be >= 0")
         self.__x = coord
 
     @property
@@ -45,4 +57,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, coord):
         """ y - sets the y coord of the rectangle """
+        if type(coord) is not int:
+            raise TypeError("y must be an integer")
+        if coord < 0:
+            raise ValueError("y must be >=0")
         self.__y = coord
