6e6aca16eff2020555a838aa64060f5b057ffb97
Jenn <jennogden95@gmail.com>
2018-10-21 18:37:34 +0000

    Updated square class to include public getter and setter size and public method update.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,13 +7,52 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ __init__ - initializes the square """
         super().__init__(size, size, x, y, id)
-        self.__width = size
-        self.__height = size
-        self.__x = x
-        self.__y = y
-        self.__id = id
+        self.size = size
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size - sets the value for size """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
         
     def __str__(self):
         """ __str__ - prints a informal description of a square instance """
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.__x, self.__y, self.__width)
+            self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """ update - updates attributes """
+        if args is not ():
+            i = 0
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                    i += 1
+                elif i == 1:
+                    self.size = arg
+                    i += 1
+                elif i == 2:
+                    self.x = arg
+                    i += 1
+                elif i == 3:
+                    self.y = arg
+                    i += 1
+        elif kwargs is not {}:
+            for arg in kwargs:
+                if arg is "id":
+                   self.id = kwargs.get(arg)
+                elif arg is "size":
+                   self.size = kwargs.get(arg)
+                elif arg is "x":
+                   self.x = kwargs.get(arg)
+                elif arg is "y":
+                   self.y = kwargs.get(arg)
+           
