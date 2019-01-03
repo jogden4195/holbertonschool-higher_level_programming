0805566932083901248a8ed23a936d04fc4f2c66
Jenn <jennogden95@gmail.com>
2019-01-03 01:32:18 +0000

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists the requested state from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    flag = 0
+    for state in session.query(State).order_by(State.id).all():
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            flag = 1
+    if flag == 0:
+        print("Not found")
+    session.close()
