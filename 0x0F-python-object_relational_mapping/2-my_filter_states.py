c5517dbe96ce7aa95718687855e90c2c50983d2a
Jenn <jennogden95@gmail.com>
2019-01-03 08:39:03 +0000

    FIX: Added charset option to connection statement and changed print format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3])
+        db=sys.argv[3],
+        charset="utf8")
     cur = conn.cursor()
     cur.execute(
         "SELECT id, name FROM states WHERE name = '{}' ORDER BY id".format(
             sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        print('{}'.format(row))
     cur.close()
     conn.close()
