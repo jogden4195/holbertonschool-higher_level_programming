17166ef661f249e753191dd3e07f983b04669d7e
Jenn <jennogden95@gmail.com>
2019-01-03 08:33:51 +0000

    FIX: Changed SELECT statement and got rid of close statements.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,10 +12,7 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute(
-        """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id""")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     query_rows = cur.fetchall()
     for row in query_rows:
         print('{}'.format(row))
-    cur.close()
-    conn.close()
