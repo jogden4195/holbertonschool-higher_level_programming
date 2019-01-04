3d40f15acb77ec79cbdef01f47a8a60480ca992a
Jenn <jennogden95@gmail.com>
2019-01-04 23:21:58 +0000

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/relationship_city_copy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    """ Class definition for City """
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                nullable=False,
+                primary_key=True,
+                unique=True,
+                autoincrement=True)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer,
+                      ForeignKey("states.id", ondelete="CASCADE"),
+                      nullable=False)
