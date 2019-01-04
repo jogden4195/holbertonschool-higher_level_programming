1a1cd7b0fe0c015068f0c80f32a4330cf84d995b
Jenn <jennogden95@gmail.com>
2019-01-04 23:22:41 +0000

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/relationship_state_copy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class definition for State """
+    __tablename__ = 'states'
+    id = Column('id', Integer,
+                nullable=False,
+                primary_key=True,
+                unique=True,
+                autoincrement=True)
+    name = Column('name', String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan", backref="states")
