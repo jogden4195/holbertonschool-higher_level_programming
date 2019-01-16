2a8401920ec47edb95ff19a647bc645d2e08c615
Jenn <jennogden95@gmail.com>
2019-01-16 02:42:36 +0000

    FIX: Im dumb and forgot to put in the parameter variable

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes in an URL, sends a GET request and displays the response's body
-curl -s --header "X-HolbertonSchool-User-Id: 98"
+curl -s --header "X-HolbertonSchool-User-Id: 98" $1
