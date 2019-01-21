6f7cf2b0383b20cd7ed02b82b5b8188728f68bb3
Jenn <jennogden95@gmail.com>
2019-01-21 21:46:12 +0000

    FIX: Doesnt executes when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,9 +5,9 @@ Prints the X-Request-Id value of the given url
 import urllib.request
 import sys
 
-
-url = sys.argv[1]
-res = urllib.request.urlopen(url)
-with res as response:
-    html = dict(response.info())
-print(html.get('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = urllib.request.urlopen(url)
+    with res as response:
+        html = dict(response.info())
+    print(html.get('X-Request-Id'))
