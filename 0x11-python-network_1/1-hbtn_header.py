c9a31474ae535a3a28b398167072840b1faae7de
Jenn <jennogden95@gmail.com>
2019-01-21 21:39:50 +0000

    Initial commit

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Prints the X-Request-Id value of the given url
+"""
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+res = urllib.request.urlopen(url)
+with res as response:
+    html = dict(response.info())
+print(html['X-Request-Id'])
