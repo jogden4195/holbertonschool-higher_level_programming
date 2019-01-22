aaf2342f631735af5b46aa21a48856c637b612ee
Jenn <jennogden95@gmail.com>
2019-01-22 00:47:19 +0000

    Initial commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter
+as a parameter
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    req = requests.post(url, data={'q': q})
+    try:
+        stuff = req.json()
+        if stuff:
+            print('[{}] {}'.format(stuff.get('id'), stuff.get('name')))
+        else:
+            print('No result')
+    except ValueError:
+        print('No result')
+    except:
+        print('Not a valid JSON')
