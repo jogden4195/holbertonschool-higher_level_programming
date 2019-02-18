474b31702fb973fed68ca62016345cfdf6cb6de9
Jenn <jennogden95@gmail.com>
2019-02-18 22:57:54 +0000

    FIX: Now importing from 5-square.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (parseInt(w) > 0 && parseInt(h) > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    let str = '';
-    for (let i = 0; i < this.width; ++i) {
-      str += 'X';
-    }
-    for (let i = 0; i < this.height; ++i) {
-      console.log(str);
-    }
-  }
-  rotate () {
-    let h = this.height;
-    this.height = this.width;
-    this.width = h;
-  }
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+const oldSquare = require('./5-square');
+module.exports = class Square extends oldSquare {
   charPrint (c) {
     let character;
     let str = '';
