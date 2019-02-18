8aa6ac2179a3b86f88e3820fa9a240c4714c3d55
Jenn <jennogden95@gmail.com>
2019-02-18 22:53:43 +0000

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,48 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let str = '';
+    for (let i = 0; i < this.width; ++i) {
+      str += 'X';
+    }
+    for (let i = 0; i < this.height; ++i) {
+      console.log(str);
+    }
+  }
+  rotate () {
+    let h = this.height;
+    this.height = this.width;
+    this.width = h;
+  }
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint (c) {
+    let character;
+    let str = '';
+    if (c) {
+      character = c;
+    } else {
+      character = 'X';
+    }
+    for (let i = 0; i < this.width; ++i) {
+      str += character;
+    }
+    for (let i = 0; i < this.width; ++i) {
+      console.log(str);
+    }
+  }
+};
