faadea88531d8ac061e7008906b3d61cff537128
Jenn <jennogden95@gmail.com>
2019-02-18 21:52:46 +0000

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let str = '';
+    for (let i = 0; i < this.width; ++i) {
+      str += 'X';
+    }
+    for (let i = 0; i < this.height; ++i) {
+      console.log(str);
+    }
+  }
+  rotate () {
+    let h = this.height;
+    this.height = this.width;
+    this.width = h;
+  }
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
