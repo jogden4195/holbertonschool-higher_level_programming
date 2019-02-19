3b6a3d137a434481e8f48f00a271997af3fe6206
Jenn <jennogden95@gmail.com>
2019-02-19 01:33:37 +0000

    FIX: Changed how function increases index value.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 let myList = require('./100-data').list;
 
-let newList = myList.map(x => x * myList.indexOf(x));
+let i = -1;
+let newList = myList.map(x => x * ++i);
 
 console.log(myList);
 console.log(newList);
